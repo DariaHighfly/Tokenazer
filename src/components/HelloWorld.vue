821d667fb391be0378f38d0ef2d83d4a32f8f95f
Daria Obrezkova <dariahighfly@gmail.com>
2019-09-27 23:32:50 +0300

    Adding app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,198 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="app">
+    <header>
+      <h2>ТОКЕНИЗАТОР</h2>
+    </header>
+    <div class="form">
+      <p>Выберите, как вы хотите разделить текст:</p>
+      <div class="form__button-default">
+        <button
+          class="form__button"
+          @click="
+                tokenType = 'default';
+                tokenValue = 'word';">
+          На слова
+        </button>
+        <button
+          class="form__button"
+          @click="
+                tokenType = 'default';
+                tokenValue = 'sentence';">
+          На предложения
+        </button>
+        <button
+          class="form__button"
+          @click="tokenType = 'default';
+                tokenValue = 'line'">
+          На строки
+        </button>
+      </div>
+      <div class="form__buttons-custom">
+        <p
+          v-if="!isCustomTokenType"
+          class="button-change-type"
+          @click="isCustomTokenType = true">
+          <u>Расширенные настройки</u>
+        </p>
+        <button
+          class="form__button"
+          v-if="isCustomTokenType"
+          @click="tokenType = 'custom'; tokenValue = 'space'">
+          По пробелам
+        </button>
+        <button
+          class="form__button"
+          v-if="isCustomTokenType"
+          @click="tokenType = 'custom'; tokenValue = 'symbol'">
+          По знакам препинания и пробелам
+        </button>
+      </div>
+      <p
+        v-if="isCustomTokenType"
+        class="button-change-type"
+        @click="isCustomTokenType = false">
+        <u>Скрыть</u>
+      </p>
+      <textarea v-if="tokenType" class="form_textarea" @change="inputText = $event.target.value">
+      </textarea>
+      <button
+        v-if="tokenType"
+        class="form__button"
+        @click="startTokenization">
+        Разбить на токены
+      </button>
+      <div v-if="tokensArray.length">
+        <p class="tokens-array__title">Результат:</p>
+        <p
+          class="form_textarea">
+          {{ tokensArray }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+
+  export default {
+    name: 'app',
+    components: {
+    },
+    data() {
+      return {
+        isCustomTokenType: false,
+        tokenType: "",
+        tokenValue: "",
+        inputText: "",
+        tokensArray: [],
+      }
+    },
+    methods: {
+      startTokenization() {
+        let type = this.tokenType;
+        let value = this.tokenValue;
+        if (type === 'default') {
+          if (value === 'word') {
+            this.parseDefaultByWord();
+          } else if (value === 'sentence') {
+            this.parseDefaultBySentences();
+          } else if (value === 'line') {
+            this.parseDefaultByLine();
+          }
+        } else if (type === 'custom') {
+          if (value === 'space') {
+            this.parseCustomBySpace();
+          } else if (value === 'symbol') {
+            this.parseCustomBySymbol();
+          }
+        }
+      },
+      parseDefaultByWord() {
+        //this.tokensArray = this.inputText.split(' ');
+      },
+      parseDefaultBySentences() {
+        //this.tokensArray = this.inputText.split(' ');
+      },
+      parseDefaultByLine() {
+        this.tokensArray = this.inputText.split('\n');
+      },
+      parseCustomBySpace() {
+        this.tokensArray = this.inputText.split(' ');
+      },
+      parseCustomBySymbol() {
+        let reg = ",";
+        this.tokensArray = this.inputText.split(reg);
+      },
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  h2 {
+    font-family: "Andale Mono";
+    font-size: 40px;
+    color: lightseagreen;
+  }
+
+  header {
+    text-align: center;
+  }
+
+  .form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .form__buttons-default,
+  .form__buttons-custom {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .form__button,
+  .button-change-type,
+  .form_textarea {
+    outline: none;
+  }
+
+  .form__button {
+    padding: 7px;
+    margin: 10px;
+    font-size: 15px;
+    background-color: lightseagreen;
+    color: white;
+    width: 180px;
+
+    border-radius: 7px 7px 7px 7px;
+    -moz-border-radius: 7px 7px 7px 7px;
+    -webkit-border-radius: 7px 7px 7px 7px;
+    border: 4px solid gold;
+
+    cursor: pointer;
+  }
+
+  .form__button:hover {
+    background-color: #1c867e;
+  }
+
+  .button-change-type:hover{
+    cursor: pointer;
+  }
+
+  .form_textarea {
+    height: 300px;
+    width: 600px;
+    overflow: scroll;
+    padding: 10px;
+
+    border-radius: 7px 7px 7px 7px;
+    -moz-border-radius: 7px 7px 7px 7px;
+    -webkit-border-radius: 7px 7px 7px 7px;
+    border: 4px solid lightseagreen;
+  }
+
+  .tokens-array__title {
+    text-align: center;
+  }
 </style>
